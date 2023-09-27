db06525d48c416df494ab5742e4d7d85e0a93d28
TibidoX <vovaboch0@gmail.com>
2023-09-27 21:30:35 +0300

    1 task

---
+++ Client/AppClient.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "AppClient.h"
+#include "helpers/SocketClient.h"
+#include "helpers/UtilString.h"
+#include "helpers/UtilFile.h"
+#include <map>
+//#include <Windows.h>
+
+bool Client::send(const std::string& url, const std::string& msg)
+{
+    SocketClient s;
+    while (!s.init() || !s.connect(url))
+        //return false;
+        Sleep(1000);
+
+    printf("sending text message \"%s\"\n", msg.c_str());
+    int len = s.sendStr(msg);
+
+    printf("sent %d bytes\n", len);
+    return len > 0;
+}
