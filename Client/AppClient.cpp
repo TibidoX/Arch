8234e4607f400c8b5f5599388dda928325115e4d
TibidoX <vovaboch0@gmail.com>
2023-09-29 22:06:14 +0300

    29.09

--- Client/AppClient.cpp
+++ Client/AppClient.cpp
@@ -4,14 +4,18 @@
 #include "helpers/UtilString.h"
 #include "helpers/UtilFile.h"
 #include <map>
+#include <chrono>
 //#include <Windows.h>
 
 bool Client::send(const std::string& url, const std::string& msg)
 {
     SocketClient s;
-    while (!s.init() || !s.connect(url))
-        //return false;
-        Sleep(1000);
+    auto start = std::chrono::steady_clock::now();
+    while ((!s.init() || !s.connect(url))&& std::chrono::duration_cast<std::chrono::seconds>(
+        std::chrono::steady_clock::now()-start).count()<60) {
+        Sleep(10);
+    }
+
 
     printf("sending text message \"%s\"\n", msg.c_str());
     int len = s.sendStr(msg);
